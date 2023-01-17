5013c268b34693d432f931f376d7771aed27bd00
Oleg <sagello@rambler.ru>
2023-01-17 23:29:26 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,38 @@
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+};
+
+const BaseUrl = "http://localhost:5400";
+//const BaseUrl = "https://www.repairtoolbox.com";
+
+const API = BaseUrl + "/api";
+const debug = true;
+
+function get(url) {
+    return new Promise(function (succeed, fail) {
+        var req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.addEventListener("load", function () {
+            if (req.status < 400)
+                succeed(req.responseText);
+            else
+                fail(req);
+        });
+        req.addEventListener("error", function () {
+            fail(new Error("Network error"));
+        });
+        req.send(null);
+    });
+}
+
+function getParams() {
+    var $_GET = [];
+    var parts = window.location.search.substr(1).split("&");
+    for (var i = 0; i < parts.length; i++) {
+        var temp = parts[i].split("=");
+        $_GET[decodeURIComponent(temp[0]).toLowerCase()] = decodeURIComponent(temp[1]);
+    }
+    return $_GET;
+}
